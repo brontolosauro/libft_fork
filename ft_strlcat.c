ba6525beec25a377d820a385e7c50eb9edcab765
Riccardo Fani <rfani@c2r6p1.42firenze.it>
2024-12-10 17:14:49 +0100

    ancora non ci siamo per nulla

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,14 +6,35 @@
 /*   By: rfani <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 10:59:05 by rfani             #+#    #+#             */
-/*   Updated: 2024/12/02 16:26:37 by rfani            ###   ########.fr       */
+/*   Updated: 2024/12/10 17:10:33 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+//#include <stdio.h>
+//#include <bsd/string.h>
+//
+//size_t	ft_strlcat(char *dst, const char *src, size_t size);
+//
+//int	main(void)
+//{
+//	char	*src = "pqrstuvwz";
+//	char	*dst = "abcd";
+//	size_t	size = 0;
+//	size_t	res;
+//	while (size < 11)
+//	{
+//		res = ft_strlcat(src, dst, size);
+//		printf("ft_strlcat(\"%s\", \"%s\", %lu) = %lu\n", src, dst, size, res);
+//		res = ft_strlcat(src, dst, size);
+//		printf("strlcat(\"%s\", \"%s\", %lu) = %lu\n", src, dst, size, res);
+//		size++;
+//	}
+//	return (0);
+//}
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	dst_len;
@@ -22,14 +43,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (dstsize != 0 && dst_len <= dstsize)
+	if (size > dst_len + 1)
 	{
-		while (i < (dstsize - dst_len - 1) && src[i] != '\0')
+		while (i < (size - dst_len - 1) && src[i] != '\0')
 		{
 			dst[dst_len + i] = src[i];
 			i++;
 		}
-		dst[dst_len + i + 1] = '\0';
+		if (src[i] == '\0')
+		{
+			dst[dst_len + i + 1] = '\0';
+			return (dst_len + src_len);
+		}
+		else
+			return (size);
 	}
-	return (dst_len + src_len);
+	return (size);
 }
