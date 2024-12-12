bb6256822229f6440b12f11c86c088de426afe3b
ricca@mignolo <riccardofani@gmail.com>
2024-12-12 23:16:31 +0100

    ft_memcpy.c passati tutti i test di paco

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -18,7 +18,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && dest != src)
 	{
 		*((unsigned char *)dest + i) = *((unsigned char *)src + i);
 		i++;
