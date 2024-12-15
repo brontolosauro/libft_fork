e3bb6af42bc71e5cbd0529c3cf0d36e24b185f64
ricca@mignolo <riccardofani@gmail.com>
2024-12-15 18:41:28 +0100

    ft_strncmp.c segfault safe

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,6 +17,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (-1);
+	if (!s2)
+		return (1);
+	if (!s1)
+		return (-1);
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
 		i++;
