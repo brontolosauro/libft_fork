e7a5d68351c522ee8b9fbf1b92b85e5ca187e5be
ricca@mignolo <riccardofani@gmail.com>
2024-12-15 17:53:39 +0100

    ft_memcpy.c segfault safe

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dest || !src)
+		return (dest);
 	while (i < n && dest != src)
 	{
 		*((unsigned char *)dest + i) = *((unsigned char *)src + i);
