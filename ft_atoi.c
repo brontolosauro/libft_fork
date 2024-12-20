9e495795d6282b9a474f35b5bc8d47c2befd78e2
ricca <riccardofani@gmail.com>
2024-12-20 16:50:38 +0100

    ft_atoi.c: complete!

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,39 +6,83 @@
 /*   By: rfani <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 10:57:29 by rfani             #+#    #+#             */
-/*   Updated: 2024/12/02 11:09:48 by rfani            ###   ########.fr       */
+/*   Updated: 2024/12/20 16:36:19 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 //
-//int	ft_atoi(const char *str);
+// int	ft_atoi(const char *str);
+int	pramble(const char *str, int *i, int *sign);
+int	ft_isspace(char c);
+int	issign(const char *str, int *i, int *sign);
 //
-//int main()
-//{
-//	char str[] = "Today's temperature is 24 degrees";
-//	printf("str[]=\"%s\"\n", str);
-//	int amount = ft_atoi(str);
-//	printf("ft_atoi(str) = %d\n", amount);
+// int main()
+// {
+// 	char str[] = "";
+// 	printf("str[]=\"%s\"\n", str);
+// 	int amount = ft_atoi(str);
+// 	printf("ft_atoi(str) = %d\n", amount);
+//     amount = atoi(str);
+//     printf("atoi(str) = %d\n", amount);
 //
-//	return (0);
-//}
+// 	return (0);
+// }
 
 int	ft_atoi(const char *str)
 {
 	int	i;
 	int	n;
+	int	sign;
 
 	i = 0;
 	n = 0;
-	while (str[i] != '\0')
+	sign = 1;
+	if (!str)
+		return (0);
+	if (!pramble(str, &i, &sign))
+		return (0);
+	while (ft_isdigit(str[i]) && str[i])
 	{
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			n = 10 * n + (str[i] - '0');
-		}
+		n = n * 10 + (str[i] - '0');
 		i++;
 	}
-	return (n);
+	return (sign * n);
+}
+
+int	pramble(const char *str, int *i, int *sign)
+{
+	while (ft_isspace(str[*i]) && str[*i])
+		*i += 1;
+	if (issign(str, i, sign) && str[*i])
+		*i += 1;
+	if (ft_isdigit(str[*i]) && str[*i])
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
+		|| c == '\r')
+		return (1);
+	return (0);
+}
+
+int	issign(const char *str, int *i, int *sign)
+{
+	if (str[*i] == '-')
+	{
+		*sign = -1;
+		return (1);
+	}
+	else if (str[*i] == '+')
+	{
+		*sign = 1;
+		return (1);
+	}
+	return (0);
 }
