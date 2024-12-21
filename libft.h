45f46ebb328efb4eb4e1255c0a6606dc21cf77f0
ricca@pinky <riccardofani@gmail.com>
2024-12-21 09:58:23 +0100

    ft_putnbr_fd.c: complete!

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rfani <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:03:25 by rfani             #+#    #+#             */
-/*   Updated: 2024/12/21 09:18:28 by rfani            ###   ########.fr       */
+/*   Updated: 2024/12/21 09:26:51 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
